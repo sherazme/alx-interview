3f8159c59d6dc5e803d3ac1bfab03fc5f6545c33
Shiraz Mohammed Elhassan Osman Elmahi <73830525+sherazme@users.noreply.github.com>
2024-10-28 04:00:21 +0200

    log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,48 +1,44 @@
 #!/usr/bin/python3
 
+
 import sys
 
 
-def display_msg(status_code, total_size):
-    """ print message """
-
-
-    print("File size: {}".format(total_size))
-    for key, val in sorted(status_code.items()):
-        if val != 0:
-            print("{}: {}".format(key, val))
-
-
+i = 0
 total_size = 0
-code = 0
-count = 0
-status_code = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+status_code = {'200': 0,
+               '301': 0,
+               '400': 0,
+               '401': 0,
+               '403': 0,
+               '404': 0,
+               '405': 0,
+               '500': 0}
 
 try:
     for line in sys.stdin:
-        parsed_line = line.split()
-        parsed_line = parsed_line[::-1]
-
-        if len(parsed_line) > 2:
-            count += 1
-
-            if count <= 10:
-                total_size += int(parsed_line[0])
-                code = parsed_line[1]
-
-                if (code in status_code.keys()):
-                    status_code[code] += 1
-
-            if (count == 10):
-                display_msg(status_code, total_size)
-                count = 0
-
+        arg = line.split(' ')
+        if len(arg) > 2:
+            status_line = arg[-2]
+            file_size = arg[-1]
+            if status_line in status_code:
+                status_code[status_line] += 1
+            total_size += int(file_size)
+            i += 1
+            if i == 10:
+                print('File size: {:d}'.format(total_size))
+                s_keys = sorted(status_code.keys())
+                for key in s_keys:
+                    value = status_code[key]
+                    if value != 0:
+                        print('{}: {}'.format(key, value))
+                i = 0
+except Exception:
+    pass
 finally:
-    display_msg(status_code, total_size)
+    print('File size: {:d}'.format(total_size))
+    s_keys = sorted(status_code.keys())
+    for key in s_keys:
+        value = status_code[key]
+        if value != 0:
+            print('{}: {}'.format(key, value))
